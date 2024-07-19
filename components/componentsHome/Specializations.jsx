8e797d9fdcd2acd7e6a371d24cc2c9b1c077a8c6
[Ayrak] <[m.treizebre@gmail.com]>
2024-07-19 11:38:14 +0200

    ajout des photos des specialisations

--- components/componentsHome/Specializations.jsx
+++ components/componentsHome/Specializations.jsx
@@ -2,7 +2,6 @@ import {useState, useEffect, useRef} from "react"
 import Link from 'next/link'
 import Image from 'next/image'
 import axios from 'axios'
-import { config } from "@/config"
 import "@/styles/specializations.css"
 
 const Specializations = () => {
@@ -32,7 +31,7 @@ const Specializations = () => {
                             <Link href={`professionnels/${spe.id}`} 
                                   aria-label={`Visiter la page des ${spe.name_spe}`}
                             >
-                                <Image src={config.pict_url+spe.picture} className="img-spe" alt={spe.name_spe} width={300} height={300}/>
+                                <Image src={spe.picture} className="img-spe" alt={spe.name_spe} width={300} height={300}/>
                                 <p>{spe.name_spe}</p>
                             </Link>
                         </div>
